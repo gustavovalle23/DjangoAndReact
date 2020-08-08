5c20adbcd80596ad3b767a48e88884685b04b7da
gustavovalle23 <gustavo.vallerp@hotmail.com>
2020-08-08 13:06:46 -0300

    Adding a new Task

--- todo_drf/api/urls.py
+++ todo_drf/api/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.apiOverView, name='api-overview'),
     path('task-list/', views.taskList, name='task-list'),
     path('task-detail/<str:pk>/', views.taskDetail, name='task-detail'),
-    path('task-create', views.taskCreate, name='task-create'),
+    path('task-create/', views.taskCreate, name='task-create'),
     path('task-update/<str:pk>/', views.taskUpdate, name='task-update'),
     path('task-delete/<str:pk>/', views.taskDelete, name='task-delete'),   
 ]

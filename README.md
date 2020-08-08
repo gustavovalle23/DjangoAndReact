0cf4aa78049185080a63f1c5e1cb63d4e0aa3c74
gustavovalle23 <gustavo.vallerp@hotmail.com>
2020-08-08 03:16:15 -0300

    Primeiras Configurações com a API e o FrontEnd

---
+++ README.md
@@ -0,0 +1,5 @@
+# Django/React
+
+### Aplicação feita com Django no Backend e React no Frontend
+
+Code base: https://github.com/divanov11/React-Django-To-Do-App/tree/master/frontend
---
+++ frontend/src/App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        todoList: [],
+        activeItem: {
+          id: null,
+          title: '',
+          completed: false,   
+        },
+        editing: false,
+      }
+      this.fetchTasks = this.fetchTasks.bind(this)
+  };
+
+
+  componentWillMount() {
+    this.fetchTasks()
+  }
+
+  fetchTasks() {
+    console.log('Fetching...');
+
+    fetch('http://127.0.0.1:8000/api/task-list/')
+      .then(response => response.json())
+      .then(data => 
+        console.log('Data:', data)
+        )
+  }
+
+
+  render() {
+
+    return (
+      <div className="container">
+
+        <div id="task-container">
+          <div id="form-wrapper">
+            <form id="form">
+              <div className="flex-wrapper">
+                <div style={{ flex: 6 }}>
+                  <input className="form-control" id="title" type="text" name="title" placeholder="Add task.." />
+                </div>
+                <div style={{ flex: 1 }}>
+                  <input id="submit" className="btn btn-warning" type="submit" name="add" value="Enviar" />
+                </div>
+              </div>
+            </form>
+
+          </div>
+
+          <div id="list-wrapper">
+
+          </div>
+
+        </div>
+
+      </div>
+    )
+
+
+  }
+}
+
+
+
+
+export default App;
